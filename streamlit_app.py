e4a60c4280a4b3f44c095c4216e1a004f903c92c
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-05-20 13:37:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,10 @@ from langchain import OpenAI
 
 st.title('🦜🔗 Quickstart App')
 
-api_key = st.text_input('Enter OpenAI API key:', type='password')
-if api_key:
-  os.environ['OPENAI_API_KEY'] = api_key
-
 with st.form('my_form'):
   llm = OpenAI(temperature=0.7)
+  api_key = st.text_input('Enter OpenAI API key:', type='password')
+  os.environ['OPENAI_API_KEY'] = api_key
   text = st.text_area('Enter text:', 'What are 3 key advice for learning how to code?')
   submitted = st.form_submit_button('Submit')
   if submitted:
