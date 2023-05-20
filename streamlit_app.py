5b88d574a33427401be77290bcd688dc767457da
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-05-20 13:39:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,8 @@ st.title('🦜🔗 Quickstart App')
 with st.form('my_form'):
   llm = OpenAI(temperature=0.7)
   api_key = st.text_input('Enter OpenAI API key:', type='password')
-  os.environ['OPENAI_API_KEY'] = api_key
   text = st.text_area('Enter text:', 'What are 3 key advice for learning how to code?')
   submitted = st.form_submit_button('Submit')
   if submitted:
+    os.environ['OPENAI_API_KEY'] = api_key
     st.info(llm(text))
