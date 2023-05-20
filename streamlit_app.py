64f40d36cb0623819b0cb487387b0b26334bc23b
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-05-20 13:44:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,15 @@ from langchain import OpenAI
 
 st.title('🦜🔗 Quickstart App')
 
-with st.form('my_form'):
+api_key = st.text_input('Enter OpenAI API key:', type='password')
+
+def generate_response(input_text):
   llm = OpenAI(temperature=0.7)
-  api_key = st.text_input('Enter OpenAI API key:', type='password')
+  os.environ['OPENAI_API_KEY'] = api_key
+  st.info(llm(input_text))
+
+with st.form('my_form'):
   text = st.text_area('Enter text:', 'What are 3 key advice for learning how to code?')
   submitted = st.form_submit_button('Submit')
   if submitted:
-    os.environ['OPENAI_API_KEY'] = api_key
-    st.info(llm(text))
+    generate_response(text)
