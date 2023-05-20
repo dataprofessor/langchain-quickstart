4fdc2bd0f3c6eb63fe06138e3515639cb49ffe32
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-05-20 13:28:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
+import os
+from langchain import OpenAI
 
-st.title('🎈 App Name')
+st.title('🦜🔗 Quickstart App')
 
-st.write('Hello world!')
+api_key = st.text_input('Enter OpenAI API key:', type='password')
+os.environ['OPENAI_API_KEY'] = api_key
+
+llm = OpenAI(temperature=0.7)
+text = st.text_input('Enter text:', 'What are 3 key advice for learning how to code?')
+st.info(llm(text))
