6c82d1fcd28cb2ad8da6e555189cebeacbc8516d
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-05-31 06:46:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,5 +12,7 @@ def generate_response(input_text):
 with st.form('my_form'):
   text = st.text_area('Enter text:', 'What are 3 key advice for learning how to code?')
   submitted = st.form_submit_button('Submit')
+  if not openai_api_key.startswith('sk-'):
+    st.warning('Please enter your OpenAI API key!', icon='⚠')
   if submitted and openai_api_key.startswith('sk-'):
     generate_response(text)
