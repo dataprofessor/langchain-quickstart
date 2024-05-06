557d0c0d895997e6530c052e4d9ab488b815eaee
Ching-Yi <82449718+ChingYi-AX@users.noreply.github.com>
2024-05-06 17:37:09 +0000

    first trz

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,17 @@
 import streamlit as st
 from langchain.llms import OpenAI
 st.set_page_config(page_title="🦜🔗 Quickstart App")
-st.title('🦜🔗 Quickstart App')
+st.title('🦜🔗 Text Quality Assurance App')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
 def generate_response(input_text):
-  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+  llm = OpenAI(temperature=0.5, openai_api_key=openai_api_key)
   st.info(llm(input_text))
 
 with st.form('my_form'):
   text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
-  submitted = st.form_submit_button('Submit')
+  submitted = st.form_submit_button('Start Quality Assurance')
   if not openai_api_key.startswith('sk-'):
     st.warning('Please enter your OpenAI API key!', icon='⚠')
   if submitted and openai_api_key.startswith('sk-'):
